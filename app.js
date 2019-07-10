f5a595b630e7db1f5b0c7509738769de598bc682
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-07-10 00:14:03 -0400

    fixed errant form termination tag that prevented proper data submission

--- app.js
+++ app.js
@@ -4,22 +4,33 @@ document
   .addEventListener("submit", calculateResults);
 
 //
-function calculateResults(e) {
-  console.log("Calculating...");
+function calculateResults(event) {
+  event.preventDefault();
+  console.log("Calculating results...");
 
   // Set DOM selector variables
-  const loanAmountDOM = document.getElementById("amount");
-  const interestRateDOM = document.getElementById("interest-rate");
-  const repaymentYearsDOM = document.getElementById("years");
-  const monthlyPaymentDOM = document.getElementById("monthly-payment");
-  const totalPaymentDOM = document.getElementById("total-payment");
-  const totalInterestDOM = document.getElementById("total-interest");
+  const loanAmount = document.getElementById("amount");
+  const interestRate = document.getElementById("interest-rate");
+  const repaymentYears = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 
   //Set value variables
+  const principle = parseFloat(loanAmount.value);
+  const calculatedInterest = parseFloat(interestRate.value) / 100 / 12;
+  const calculatedPayments = parseFloat(repaymentYears.value) * 12;
 
-  const loanAmount = parseFloat(loanAmountDOM.value);
-  const interestRate = parseFloat(interestRateDOM.value);
-  const principle = parseFloat(loanAmountDOM);
+  //Compute monthly payment
 
-  e.preventDefault();
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principle * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principle).toFixed(2);
+  } else {
+    console.log("check your numbers");
+  }
 }
