e46aedd006c7313af8b9e7c838549732e20ebcf1
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-07-11 22:45:02 -0400

    added conditional calculation using radio button and event listeners

--- leverageRatio.js
+++ leverageRatio.js
@@ -4,26 +4,74 @@ document.getElementById("loading").style.display = "none";
 
 const calculateResults = function(event) {
   event.preventDefault();
+
+  //Trigger calculations based on radio button status
+  document.getElementById("income-radio").checked
+    ? leverageRatioIncome()
+    : leverageRatioEquity();
   let income = parseFloat(document.getElementById("income").value, 10);
+  let equity = parseFloat(document.getElementById("equity").value, 10);
   let debtPayments = parseFloat(
     document.getElementById("debt-payments").value,
     10
   );
+
   document.getElementById("loading").style.display = "block";
-
   setTimeout(() => {
-    let cashflow = debtPayments / income;
-    let leverageRatioText = document.getElementById("leverage-ratio");
-    leverageRatioText.value = cashflow.toFixed(2) * 100 + "%";
-
-    cashflow.toFixed(2) < 0.33
-      ? (leverageRatioText.style.color = "green")
-      : (leverageRatioText.style.color = "red");
-
     document.getElementById("loading").style.display = "none";
-    document.getElementById("results").style.display = "block";
   }, 2000);
+
+  function leverageRatioEquity() {
+    setTimeout(() => {
+      let leverageRatio = debtPayments / equity;
+      let leverageRatioText = document.getElementById("leverage-ratio");
+      leverageRatioText.value = leverageRatio.toFixed(2) * 100 + "%";
+
+      leverageRatio.toFixed(2) < 0.33
+        ? (leverageRatioText.style.color = "green")
+        : (leverageRatioText.style.color = "red");
+
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("results").style.display = "block";
+    }, 2000);
+  }
+  function leverageRatioIncome() {
+    setTimeout(() => {
+      let leverageRatio = debtPayments / income;
+      let leverageRatioText = document.getElementById("leverage-ratio");
+      leverageRatioText.value = leverageRatio.toFixed(2) * 100 + "%";
+
+      //turn red or green depending on recommended threshold
+      leverageRatio.toFixed(2) < 0.33
+        ? (leverageRatioText.style.color = "green")
+        : (leverageRatioText.style.color = "red");
+
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("results").style.display = "block";
+    }, 2000);
+  }
 };
+
+function calcChoose(event) {
+  event.preventDefault();
+  //clear all values
+  document.getElementById("income").value = 0;
+  document.getElementById("equity").value = 0;
+  document.getElementById("debt-payments").value = 0;
+  console.log("Calculator Choosen");
+
+  if (document.getElementById("income-radio").checked) {
+    console.log("using Income Calculator");
+    document.getElementById("equity-group").style.display = "none";
+    document.getElementById("income-group").style.display = "block";
+  } else if (document.getElementById("equity-radio").checked) {
+    document.getElementById("equity-group").style.display = "block";
+    document.getElementById("income-group").style.display = "none";
+    console.log("using Equity calculator ");
+  }
+}
+
 document
   .getElementById("leverage-ratio-form")
   .addEventListener("submit", calculateResults);
+document.getElementById("calc-choose").addEventListener("change", calcChoose);
